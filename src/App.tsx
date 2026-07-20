38799b74c368359c9a4308e71f43d1d1088f5a39
m3d-hdj <m3dhdj@gmail.com>
2026-07-20 18:42:45 +0100

    [skip netlify] feat: mount custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Admin from './pages/Admin'
+import Cursor from './components/Cursor'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <Cursor />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   )
 }
